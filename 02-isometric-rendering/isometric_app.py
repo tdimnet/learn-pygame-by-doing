f992d6a96f5194ee3de53c2d0ddda83b7d05aa1e
Thomas Dimnet <thomas.dimnet@gmail.com>
2026-03-01 16:14:15 +0100

    Finishing writing the article

--- 02-isometric-rendering/isometric_app.py
+++ 02-isometric-rendering/isometric_app.py
@@ -19,37 +19,17 @@ pygame.display.set_caption("My First Game")
 clock = pygame.time.Clock()
 
 
-def draw_tile_topdown(
-    surface: pygame.Surface,
-    gx: int,
-    gy: int,
-    color: tuple[int, int, int]
-) -> None:
-    x = gx * TILE_SIZE + 200 # offset to center the grid
-    y = gy * TILE_SIZE + 100
+def screen_to_grid(
+    screen_x: int,
+    screen_y: int    
+) -> tuple[int, int]:
+    x = screen_x- SCREEN_WIDTH // 2
+    y = screen_y - 200
 
-    rect = pygame.Rect(x, y, TILE_SIZE, TILE_SIZE)
-    pygame.draw.rect(surface, color, rect) # Draw the title
-    pygame.draw.rect(
-        surface,
-        (0, 0, 0),
-        rect,
-        1
-    ) # Draw the border around the tile
+    gx = (y / (TILE_HEIGHT / 2) + x / (TILE_WIDTH / 2)) / 2
+    gy = (y / (TILE_HEIGHT / 2) - x / (TILE_WIDTH / 2)) / 2
 
-
-def get_tile_at_mouse(
-        mouse_x: int,
-        mouse_y: int
-) -> tuple[int, int] | None:
-    # Reverse the calculation from draw_tilte_topdown
-    gx = (mouse_x - 200) // TILE_SIZE
-    gy = (mouse_y - 100) // TILE_SIZE
-
-    if 0 <= gx < GRID_SIZE and 0 <= gy < GRID_SIZE:
-        return gx, gy
-    
-    return None
+    return int(gx), int(gy)
 
 
 def grid_to_iso(gx: int, gy: int) -> tuple[int, int]:
@@ -88,12 +68,21 @@ while running:
     screen.fill((30, 30, 30))
 
     mouse_x, mouse_y = pygame.mouse.get_pos()
-    hovered_tile = get_tile_at_mouse(mouse_x, mouse_y)
+    gx, gy = screen_to_grid(mouse_x, mouse_y)
+
+    hovered_tile = None
+    if 0 <= gx < GRID_SIZE and 0 <= gy < GRID_SIZE:
+        hovered_tile = (gx, gy)
 
     # Draw all tiles
     for gx in range(GRID_SIZE):
         for gy in range(GRID_SIZE):
-            draw_tile_iso(screen, gx, gy, (100, 150, 100))
+            color = (100, 150, 100)
+            
+            if hovered_tile and hovered_tile == (gx, gy):
+                color = (150, 200, 150)  # lighter green for hover
+            
+            draw_tile_iso(screen, gx, gy, color)
 
     pygame.display.flip()
     clock.tick(60)
