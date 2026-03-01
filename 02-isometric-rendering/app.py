1d880985710363f45c587492e53daad05e0a825c
Thomas Dimnet <thomas.dimnet@gmail.com>
2026-03-01 12:44:21 +0100

    Draw the tile on a 2D visual

--- 02-isometric-rendering/app.py
+++ 02-isometric-rendering/app.py
@@ -16,8 +16,23 @@ pygame.display.set_caption("My First Game")
 clock = pygame.time.Clock()
 
 
-def draw_tite_topdown() -> None:
-    pass
+def draw_tite_topdown(
+    surface: pygame.Surface,
+    gx: int,
+    gy: int,
+    color: tuple[int, int, int]
+) -> None:
+    x = gx * TITLE_SIZE + 200 # offset to center the grid
+    y = gy * TITLE_SIZE + 100
+
+    rect = pygame.Rect(x, y, TITLE_SIZE, TITLE_SIZE)
+    pygame.draw.rect(surface, color, rect) # Draw the title
+    pygame.draw.rect(
+        surface,
+        (0, 0, 0),
+        rect,
+        1
+    ) # Draw the border around the tile
 
 
 def get_tile_at_mouse() -> None:
@@ -31,6 +46,10 @@ while running:
             running = False
         
     screen.fill((30, 30, 30))
+
+    for gx in range(GRID_SIZE):
+        for gy in range(GRID_SIZE):
+            draw_tite_topdown(screen, gx, gy, (100, 150, 100))
 
     pygame.display.flip()
     clock.tick(60)
