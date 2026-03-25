8698c164bb2898d2cea8cdb905803ff5b266469d
Thomas Dimnet <thomas.dimnet@gmail.com>
2026-03-25 18:58:03 +0100

    Start to work on displaying the sprite on screen

---
+++ 10-bonus-fps-with-pygame/rendering/sprite_renderer.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+import math
+import pygame
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from engine.enemy import Enemy
+    from engine.player import Player
+
+from config import (
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,SCREEN_DIST,
+    HALF_FOV
+)
+
+
+class SpriteRenderer:
+    def __init__(self):
+        pass
+
+    def render(self):
+        pass
