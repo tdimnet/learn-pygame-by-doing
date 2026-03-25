dd06ad3a930234cc2a78b74a98a05f6502ff7964
Thomas Dimnet <thomas.dimnet@gmail.com>
2026-03-25 09:20:01 +0100

    Working on the Game object

---
+++ 10-bonus-fps-with-pygame/game.py
@@ -0,0 +1,25 @@
+import pygame
+
+from config import (
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT
+)
+from engine.map import Map
+from engine.player import Player
+
+
+class Game:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def handle_events(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
